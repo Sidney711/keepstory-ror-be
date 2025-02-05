448dac2bc1afd25057f2a635ccce045b055290ec
Zdeněk Carbol <zdenek.carbol.711@gmail.com>
2025-02-05 14:27:17 +0100

    Update cors and genereate rodauth mailer

--- app/misc/rodauth_main.rb
+++ app/misc/rodauth_main.rb
@@ -43,6 +43,17 @@ class RodauthMain < Rodauth::Rails::Auth
     # require_password_confirmation? false
     # require_login_confirmation? false
 
+    create_verify_account_email do
+      RodauthMailer.verify_account(self.class.configuration_name, account_id, verify_account_key_value)
+    end
+    create_reset_password_email do
+      RodauthMailer.reset_password(self.class.configuration_name, account_id, reset_password_key_value)
+    end
+    create_verify_login_change_email do |_login|
+      RodauthMailer.verify_login_change(self.class.configuration_name, account_id, verify_login_change_key_value)
+    end
+
+
     # Use path prefix for all routes.
     # prefix "/auth"
 
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -16,7 +16,12 @@
 # end
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
-    resource '*', headers: :any, methods: [:get, :post, :patch, :put]
+    origins ENV["FRONTEND_URL"]
+
+    resource "*",
+             headers: :any,
+             expose: ['Authorization'],
+             methods: [:get, :post, :put, :patch, :delete, :options, :head],
+             credentials: true
   end
 end
